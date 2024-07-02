fea8777626e0518aa8440d9df0cd6a2b248306c1
Nic <nicholaslucio2018@gmail.com>
2024-07-02 19:23:24 -0300

    Alterei o metodos do controller commit

--- vendamais/contoller/ProdutoController.java
+++ vendamais/contoller/ProdutoController.java
@@ -1,9 +1,11 @@
 package br.com.empresa.vendamais.contoller;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,39 +22,39 @@ import br.com.empresa.vendamais.service.ProdutoService;
 
 public class ProdutoController {
 
-        @Autowired
-        private ProdutoService produtoService;
-        @GetMapping("/listar")
-        public ResponseEntity<List<Produto>> findAll(){
-            List<Produto> produtos = produtoService.findAll();
-            return ResponseEntity.ok().body(produtos);
+    @Autowired
+    private ProdutoService produtoService;
 
-        }
-        @GetMapping("/{id}")
-        public ResponseEntity<Produto> findById(@PathVariable Long id){
-            Produto produto = produtoService.findById(id);
-            return ResponseEntity.ok().body(produto);
-            
-        }
-        @PostMapping("/inserir")
-        public ResponseEntity<Produto> insertNew(@RequestBody Produto produto){
-            Produto produtoInserido = produtoService.insertNew(produto);
-            return ResponseEntity.ok().body(produtoInserido);
-        }
-        @PutMapping("/{id}")
-        public ResponseEntity<Produto> update(@PathVariable Long id,@RequestBody Produto produtoAlterado){
-        Produto produto = produtoService.update(id, produtoAlterado);
-        return ResponseEntity.ok().body(produto);
-    
+    @GetMapping("/listar")
+    public ResponseEntity<List<Produto>> findAll() {
+        List<Produto> produtos = produtoService.findAll();
+        return ResponseEntity.ok().body(produtos);
+
     }
 
-    public ResponseEntity<Boolean> deleteById(@PathVariable Long id){
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {
+        Produto produto = produtoService.findById(id);
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @PostMapping("/inserir")
+    public ResponseEntity<Produto> insertNew(@RequestBody Produto produto) {
+        Produto produtoInserido = produtoService.insertNew(produto);
+        return ResponseEntity.ok().body(produtoInserido);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto produtoAlterado) {
+        Produto produto = produtoService.update(id, produtoAlterado);
+        return ResponseEntity.ok().body(produto);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteById(@PathVariable Long id) {
         Boolean flag = produtoService.deleteById(id);
         return ResponseEntity.ok().body(flag);
     }
-   
+
 }
-
-
-
-
